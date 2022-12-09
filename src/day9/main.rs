7e29d8edde2c12b4b23b1d860012b04983f12137
Michael Sickles <michael.c.sickles@gmail.com>
2022-12-09 15:34:41 -0500

    Unneed clone

--- src/day9/main.rs
+++ src/day9/main.rs
@@ -7,16 +7,19 @@ pub fn run(filename: String) {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
-    let commands: Vec<Command> = lines.into_iter().map(|s| Command::new(s.ok().unwrap())).collect();
+    let commands: Vec<Command> = lines
+        .into_iter()
+        .map(|s| Command::new(s.ok().unwrap()))
+        .collect();
     let mut visited_part1 = HashSet::<Coord>::new();
-    let mut rope1 = vec![Coord::new(0,0);2];
+    let mut rope1 = vec![Coord::new(0, 0); 2];
 
     let mut visited_part2 = HashSet::<Coord>::new();
-    let mut rope2 = vec![Coord::new(0,0);10];
+    let mut rope2 = vec![Coord::new(0, 0); 10];
 
     for command in commands {
-        rope1 = command.apply(&mut rope1.clone(),&mut visited_part1);
-        rope2 = command.apply(&mut rope2.clone(),&mut visited_part2);
+        command.apply(&mut rope1, &mut visited_part1);
+        command.apply(&mut rope2, &mut visited_part2);
     }
     println!("Visted p1: {}", visited_part1.len());
     println!("Visted p2: {}", visited_part2.len());
@@ -24,11 +27,11 @@ pub fn run(filename: String) {
 #[derive(Eq, Hash, PartialEq, Clone, Debug)]
 struct Coord {
     x: i32,
-    y: i32
+    y: i32,
 }
 impl Coord {
     fn new(x: i32, y: i32) -> Coord {
-        Coord { x: x, y: y}
+        Coord { x: x, y: y }
     }
     fn add(&mut self, new: Coord) {
         self.x += new.x;
@@ -40,7 +43,7 @@ impl Coord {
     fn move_to(&mut self, next: &Coord) {
         let mut x_diff = next.x - self.x;
         let mut y_diff = next.y - self.y;
-        if x_diff == 2 || x_diff == -2{
+        if x_diff == 2 || x_diff == -2 {
             x_diff = x_diff / 2;
         }
         if y_diff == 2 || y_diff == -2 {
@@ -52,27 +55,27 @@ impl Coord {
 
 struct Command {
     direction: Dir,
-    amount: i32
+    amount: i32,
 }
 impl Command {
     fn new(str: String) -> Command {
         let split: Vec<&str> = str.split(" ").collect::<Vec<&str>>();
         Command {
             direction: Dir::new(split[0]),
-            amount: split[1].parse::<i32>().unwrap()
+            amount: split[1].parse::<i32>().unwrap(),
         }
     }
-    fn apply(&self, rope: &mut Vec<Coord>, visited:&mut HashSet<Coord>) -> Vec<Coord> {
+    fn apply(&self, rope: &mut Vec<Coord>, visited: &mut HashSet<Coord>) -> Vec<Coord> {
         let size = rope.len();
         for _ in 0..self.amount {
             rope[0].add(self.direction.coord());
             for i in 1..(size) {
-                let head = rope[i-1].clone();
-                if !rope[i-1].is_touching(&rope[i]) {
+                let head = rope[i - 1].clone();
+                if !rope[i - 1].is_touching(&rope[i]) {
                     rope[i].move_to(&head);
                 }
             }
-            visited.insert(rope[size-1].clone());
+            visited.insert(rope[size - 1].clone());
         }
         rope.clone()
     }
@@ -82,7 +85,7 @@ enum Dir {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
 impl Dir {
     fn new(str: &str) -> Dir {
@@ -91,15 +94,15 @@ impl Dir {
             "D" => Dir::Down,
             "L" => Dir::Left,
             "R" => Dir::Right,
-            _ => unreachable!("Invalid Direction given: {}", str)
+            _ => unreachable!("Invalid Direction given: {}", str),
         }
     }
     fn coord(&self) -> Coord {
         match &self {
             Dir::Up => Coord::new(0, 1),
-            Dir::Down => Coord::new(0,-1),
-            Dir::Left => Coord::new(-1,0),
-            Dir::Right => Coord::new(1,0)
+            Dir::Down => Coord::new(0, -1),
+            Dir::Left => Coord::new(-1, 0),
+            Dir::Right => Coord::new(1, 0),
         }
     }
 }
